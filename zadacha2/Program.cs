c39c2dfa27b4fe3a4bcb2f04ea2117911141d18c
Maksim <qjzDyH@mail.ru>
2022-10-19 21:09:39 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("На выходе показывает третье число");
+Console.WriteLine("Введите число: ");
+int numA = int.Parse(Console.ReadLine()!);
+int thirdDigit(int numA)
+{
+    if (numA < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        while (numA >= 1000)
+        {
+            numA  /= 10;
+
+        }
+       numA %= 10;
+
+    }
+    return numA;
+   } 
+Console.WriteLine("Третья цифра числа:" + thirdDigit(numA));
