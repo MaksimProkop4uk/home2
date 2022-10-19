c359f860dc62e03c0638228e6c2d1a1c5e8d0186
Maksim <qjzDyH@mail.ru>
2022-10-19 20:23:16 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int numA = int.Parse(Console.ReadLine()!);
+int secondDigit(int numA)
+{
+ int a = numA / 10 % 10;
+    return a;
+}
+int sd= secondDigit(numA);
+Console.WriteLine("Второе число: " + sd);
