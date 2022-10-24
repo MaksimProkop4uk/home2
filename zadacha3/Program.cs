5e37fbb156fdfb7fbc46368ebf21342ba4429c4e
Maksim <qjzDyH@mail.ru>
2022-10-24 18:35:59 +0300

    задача3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите день недели: ");
+int day = int.Parse(Console.ReadLine()!);
+void weekend(int day)
+{
+    if (day > 7 || day < 1) 
+    {
+        Console.WriteLine("Пожалуйста, введите корректный день недели");
+    }
+    else if (day == 6 || day == 7)
+    {
+        Console.WriteLine("Да, это выходной");
+    }
+    else
+    { 
+        Console.WriteLine("Нет, это не выходной");
+        
+    }
+    
+}
+weekend(day);
